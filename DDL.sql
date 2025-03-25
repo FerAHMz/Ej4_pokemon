728d883f98eddf64e9df62958b86588afb0dca96
Fernando Hernandez <ferandree270904@gmail.com>
2025-03-25 13:33:25 -0600

    Update to the tables, created

--- DDL.sql
+++ DDL.sql
@@ -1,14 +1,28 @@
-create table Cartas (
-    id serial primary key NOT NULL,
-    nombre varchar(250) NOT NULL,
-    tipo varchar(250) NOT NULL,
-    rareza varchar(250) NOT NULL
+CREATE TABLE cartas (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    pokedex_number INTEGER,
+    supertype VARCHAR(255),
+    subtypes VARCHAR(255),
+    hp INTEGER,
+    types VARCHAR(255),
+    attacks VARCHAR(255),
+    weaknesses VARCHAR(255),
+    retreat_cost VARCHAR(255),
+    set_name VARCHAR(255),
+    release_date DATE,
+    artist VARCHAR(255),
+    rarity VARCHAR(255),
+    card_image_small VARCHAR(255),
+    card_image_hires VARCHAR(255),
+    tcg_player_url VARCHAR(255)
 );
 
-create table Precio (
-    id serial primary key NOT NULL,
-    carta_id int references Cartas(id) NOT NULL,
-    precio float NOT NULL,
-    fecha date NOT NULL
+CREATE TABLE precios (
+    id SERIAL PRIMARY KEY,
+    carta_id INTEGER REFERENCES cartas(id),
+    precio DECIMAL,
+    tipo_precio VARCHAR(50),
+    fecha DATE
 );
 
